dbb2166b8e5be45df30feceef49210edbee0ded6
Foxugly <rvilain@skynet.be>
2017-01-18 11:40:44 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = '-b+&3%*nxun2l+id*85aou#vpkw%uhd3ko&b06jmkr#ke)6k3k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
