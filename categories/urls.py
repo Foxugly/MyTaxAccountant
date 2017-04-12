769a07bcfa92aaaf4695347b9f75718bdf5050da
Foxugly <rvilain@skynet.be>
2017-04-12 15:15:15 +0200

    Update urls.py

--- categories/urls.py
+++ categories/urls.py
@@ -9,11 +9,12 @@
 
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
-from categories.views import category_view, add_documents, list_documents, form_document
+from categories.views import category_view, add_documents, list_documents, form_document, view_category
 
 urlpatterns = (
     url(r'^(?P<category_id>[0-9]+)/add_documents/$', login_required(add_documents), name='add_documents'),
     url(r'^(?P<category_id>[0-9]+)/list/(?P<n>[0-9]+)/$', login_required(list_documents), name='list_documents'),
     url(r'^(?P<category_id>[0-9]+)/form/(?P<n>[0-9]+)/$', login_required(form_document), name='list_documents'),
+    url(r'^(?P<category_id>[0-9]+)/$', login_required(view_category), name='view_category'),
     url(r'^(?P<category_id>[0-9]+)/$', login_required(category_view), name='category_view'),
 )
