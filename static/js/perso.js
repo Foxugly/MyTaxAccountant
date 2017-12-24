dff7115153a2d0599ecb7a6c97ad754ab0f66b1d
foxugly <rvilain@skynet.be>
2017-12-24 15:32:56 +0100

    admin to add trimesters

--- static/js/perso.js
+++ static/js/perso.js
@@ -568,7 +568,7 @@ $(document).ready(function() {
              dataType: "json",
              success: function(result){
                  if (result["valid"]) {
-                      setTimeout(window.open(result['url'], '_blank'), 1000);
+                      setTimeout(window.open(result['url'], '_blank'), 2500);
                  }
             },
             error: function(){
