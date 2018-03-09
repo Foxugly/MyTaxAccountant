c1ee207e906d5692696345fde18e0a0f0f7a1114
foxugly <rvilain@skynet.be>
2018-03-09 15:22:04 +0100

    upgrade to Pip3, Django 2.0.2

--- database.py
+++ database.py
@@ -5,13 +5,5 @@ db = {  'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(os.path.dirname(__file__), 'db.sqlite3'),
         }
-#        'default': {
-#            'ENGINE': 'django.db.backends.mysql', # 'django.db.backends.postgresql_psycopg2' for postgresql
-#            'NAME': 'DB_NAME',
-#            'USER': 'DB_USER',
-#            'PASSWORD': 'DB_PASSWORD',
-#            'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-#            'PORT': '3306',
-#        }
     }
 
