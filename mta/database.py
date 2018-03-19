310afd286f75f90ffa5a581928d552cf95ef2540
foxugly <rvilain@skynet.be>
2018-03-19 17:37:44 +0100

    add celery-backend

---
+++ mta/database.py
@@ -0,0 +1,9 @@
+
+import os
+
+db = {  'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(os.path.dirname(__file__), 'db.sqlite3'),
+        }
+    }
+
---
+++ mta/settings.py
@@ -0,0 +1,181 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2015, Foxugly. All rights reserved.
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at
+# your option) any later version.
+from __future__ import absolute_import, unicode_literals
+from django.utils.translation import ugettext_lazy as _
+from mta import database
+
+"""
+Django settings for MyTaxAccountant project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.7/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'changeme'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+ALLOWED_HOSTS = ['*']
+DATABASES = database.db
+
+# Application definition
+
+INSTALLED_APPS = (
+    'fileupload.apps.FileuploadConfig',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django_extensions',
+    'debug_toolbar',
+    'djcelery',
+    'django_celery_results',
+    'hijack',
+    'compat',
+    'hijack_admin',
+    'bootstrap3',
+    'companies',
+    'years',
+    'trimesters',
+    'categories',
+    'documents',
+    'utils',
+    'users',
+    'error',
+    'webshell',
+)
+
+MIDDLEWARE = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    #'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
+
+ROOT_URLCONF = 'mta.urls'
+WSGI_APPLICATION = 'mta.wsgi.application'
+
+UPLOAD_LOG = '/tmp/upload_log'
+
+LANGUAGE_CODE = 'fr'
+TIME_ZONE = 'Europe/Brussels'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+AUTH_PROFILE_MODULE = 'users.UserProfile'
+LOGIN_REDIRECT_URL = 'login'
+LOGIN_URL = '/user/login/'
+
+LANGUAGES = (
+  ('fr', _('Francais')),
+  ('nl', _('Nederlands')),
+  ('en', _('English')),
+)
+
+DEFAULT_LANGUAGE = 1
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+UPLOAD_DIR = 'upload'
+STOCK_DIR = 'folders'
+# ACTIVE TO PROD / COMMENT TO TEST
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+TMP_ROOT = MEDIA_ROOT + '/tmp/'
+TMP_URL = MEDIA_URL + 'tmp/'
+ADMIN_MEDIA_PREFIX = '/static/admin/'
+# COMMENT TO PROD / ACTIVE TO TEST
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        }
+    }
+}
+
+HIJACK_LOGIN_REDIRECT_URL = '/'
+HIJACK_LOGOUT_REDIRECT_URL = '/admin/auth/user/'
+HIJACK_DISPLAY_WARNINGS = True
+HIJACK_USE_BOOTSTRAP = True
+HIJACK_ALLOW_GET_REQUESTS = True
+
+
+if DEBUG:
+    def show_toolbar(request):
+        return True
+
+
+    DEBUG_TOOLBAR_CONFIG = {
+        'SHOW_TOOLBAR_CALLBACK': 'mta.settings.show_toolbar',
+    }
+
+BROKER_URL = 'redis://localhost:6379'
+# CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'django-db'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Europe/Brussels'
