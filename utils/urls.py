4ee65e580656620a5a6bbe615adbbfb088b90f3c
Foxugly <rvilain@skynet.be>
2018-03-22 16:20:44 +0100

    path

--- utils/urls.py
+++ utils/urls.py
@@ -7,15 +7,14 @@
 # the Free Software Foundation, either version 3 of the License, or (at
 # your option) any later version.
 
-from django.conf.urls import url
+from django.urls import path
 from django.contrib.auth.decorators import login_required
 from utils.views import admin_utils, add_fiscal_year, add_model_trimester, add_trimesters
 
 
 urlpatterns = (
-
-    url(r'^$', login_required(admin_utils), name='utils'),
-    url(r'^add_fiscal_year/$', login_required(add_fiscal_year), name='add_fiscal_year'),
-    url(r'^add_model_trimester/$', login_required(add_model_trimester), name='add_model_trimester'),
-    url(r'^add_trimesters/$', login_required(add_trimesters), name='add_trimesters'),
+    path('', login_required(admin_utils), name='utils'),
+    path('add_fiscal_year/', login_required(add_fiscal_year), name='add_fiscal_year'),
+    path('add_model_trimester/', login_required(add_model_trimester), name='add_model_trimester'),
+    path('add_trimesters/', login_required(add_trimesters), name='add_trimesters'),
 )
