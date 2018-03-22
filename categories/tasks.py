67bb4c76e5eb3040e8a2e41a384e102147fd9e30
Foxugly <rvilain@skynet.be>
2018-03-22 16:41:34 +0100

    logger

--- categories/tasks.py
+++ categories/tasks.py
@@ -10,6 +10,11 @@ import time
 from categories.models import Category
 from fileupload.models import FileUpload
 from documents.models import Document
+import logging
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(settings.LOGGER)
 
 
 @app.task
@@ -19,12 +24,13 @@ def add(a, b):
 
 @app.task
 def add_img(d_id, filename):
+    logger.debug("add_img | d_id = %d | filename = %s" % (d_id, filename))
     d = Document.objects.get(id=d_id)
-    if settings.DEBUG:
-        print('add_img')
+    if not d:
+        logger.error("add_img | document (id = %d) does not exist" % d_id)
     path = os.path.join(d.refer_category.get_absolute_path(), filename)
-    if not os.path.exists(path) and settings.DEBUG:
-        print("[add_img] ERROR path: %s doesn't exist" % path)
+    if not os.path.exists(path):
+        logger.error("add_img | path: %s doesn't exist" % path)
     else:
         w, h = Image.open(path).size
         d.add_page(d.get_npages() + 1, filename, w, h)
@@ -33,11 +39,16 @@ def add_img(d_id, filename):
 
 @app.task
 def add_img_document(cat_id, d_id, fu_id):
-    if settings.DEBUG:
-        print('add_img_document')
+    logger.debug("add_img_document | cat_id = %d | d_id = %d | fu_id = %d" % (cat_id, d_id, fu_id))
     cat = Category.objects.get(id=cat_id)
+    if not cat:
+        logger.error("add_img_document | Category (id = %d) does not exist" % cat_id)
     d = Document.objects.get(id=d_id)
+    if not d:
+        logger.error("add_img_document | Document (id = %d) does not exist" % d_id)
     fu = FileUpload.objects.get(id=fu_id)
+    if not fu:
+        logger.error("add_img_document | FileUpload (id = %d) does not exist" % fu_id)
     path = os.path.join(settings.MEDIA_ROOT, fu.file.name)
     new_filename = '%d_%s' % (d.id, os.path.basename(fu.file.name))
     shutil.copy(path, os.path.join(cat.get_absolute_path(), new_filename))
@@ -49,11 +60,16 @@ def add_img_document(cat_id, d_id, fu_id):
 
 @app.task
 def add_pdf_document(cat_id, d_id, fu_id):
-    if settings.DEBUG:
-        print('add_pdf_document')
+    logger.debug("add_pdf_document | cat_id = %d | d_id = %d | fu_id = %d" % (cat_id, d_id, fu_id))
     cat = Category.objects.get(id=cat_id)
+    if not cat:
+        logger.error("add_pdf_document | Category (id = %d) does not exist" % cat_id)
     d = Document.objects.get(id=d_id)
+    if not d:
+        logger.error("add_pdf_document | Document (id = %d) does not exist" % d_id)
     fu = FileUpload.objects.get(id=fu_id)
+    if not fu:
+        logger.error("add_pdf_document | FileUpload (id = %d) does not exist" % fu_id)
     path = os.path.join(settings.MEDIA_ROOT, fu.file.name)
     try:
         PdfFileReader(open(path, 'rb')).getNumPages()
@@ -86,15 +102,12 @@ def add_pdf_document(cat_id, d_id, fu_id):
     for i in range(1, 10):
         time.sleep(1)
         if os.path.exists(last_img):
-            if settings.DEBUG:
-                print("[add_pdf_document] %s exists" % last_img)
+            logger.info("add_pdf_document %s exists" % last_img)
             break
         else:
-            if settings.DEBUG:
-                print("[add_pdf_document] [%d] %s doesn't exist" % (i, last_img))
+            logger.error("add_pdf_document | %d %s does not exist" % (i, last_img))
     if not os.path.exists(last_img):
-        if settings.DEBUG:
-            print("[add_pdf_document] ERROR path: %s doesn't exist" % last_img)
+        logger.error("add_pdf_document | %s does not exist" % last_img)
     for i in range(1, n + 1):
         name_page = "%d_%03d_%s" % (d.id, i, filename)
         add_img(d_id, name_page)
@@ -106,9 +119,10 @@ def add_pdf_document(cat_id, d_id, fu_id):
 
 @app.task
 def add_spec_document(cat_id, d_id, fu_id, regex):
-    if settings.DEBUG:
-        print('add_spec_document')
+    logger.debug("add_spec_document | cat_id = %d | d_id = %d | fu_id = %d | regex = %s" % (cat_id, d_id, fu_id, regex))
     fu = FileUpload.objects.get(id=fu_id)
+    if not fu:
+        logger.error("add_spec_document | FileUpload (id = %d) does not exist" % fu_id)
     path = os.path.join(settings.MEDIA_ROOT, fu.file.name)
     filename = os.path.basename(path)
     p = re.compile(regex)
@@ -122,13 +136,11 @@ def add_spec_document(cat_id, d_id, fu_id, regex):
 
 @app.task
 def add_doc_document(cat_id, d_id, fu_id):
-    if settings.DEBUG:
-        print('add_doc_document')
+    logger.debug("add_doc_document | cat_id = %d | d_id = %d | fu_id = %d" % (cat_id, d_id, fu_id))
     return add_spec_document(cat_id, d_id, fu_id, r'.[Dd][Oo][Cc][xX]?$')
 
 
 @app.task
 def add_xls_document(cat_id, d_id, fu_id):
-    if settings.DEBUG:
-        print('add_xls_document')
+    logger.debug("add_xls_document | cat_id = %d | d_id = %d | fu_id = %d" % (cat_id, d_id, fu_id))
     return add_spec_document(cat_id, d_id, fu_id, r'.[Xx][Ll][Ss][xX]?$')
