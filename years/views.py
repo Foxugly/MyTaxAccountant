5abfd6c7d58ca457f97109552ddbec1775194db8
Foxugly <rvilain@skynet.be>
2018-03-22 15:59:46 +0100

    get_object_or_404 and logger

--- years/views.py
+++ years/views.py
@@ -6,26 +6,34 @@
 # under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or (at
 # your option) any later version.
-
+from django.conf import settings
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 from years.models import Year
 import json
+import logging
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(settings.LOGGER)
 
 
 def favorite_trimester(year):
+    logger.debug("favorite_trimester | year = %s" % year)
     t = year.trimesters.filter(active=True, favorite=True)
     if not t:
         t = [year.trimesters.filter(active=True)[0]]
     return t[0]
 
 
-def year_view(request, year_id):
-    return HttpResponse("year_view")
+#def year_view(request, year_id):
+#    return HttpResponse("year_view")
 
 
-def list_trimester(request, year_id):
+def list_trimesters(request, year_id):
+    logger.debug("list_trimesters | year_id = %d" % year_id)
     if request.is_ajax():
-        y = Year.objects.get(id=year_id)
+        y = get_object_or_404(Year, id=year_id)
         if request.user.is_superuser:
             results = {'list': [t.as_json() for t in y.trimesters.filter(active=True).order_by('template__number')],
                        'return': True, 'favorite': favorite_trimester(y).as_json()}
@@ -36,8 +44,9 @@ def list_trimester(request, year_id):
 
 
 def forward_trimester(request, year_id):
+    logger.debug("forward_trimester | year_id = %d" % year_id)
     if request.is_ajax():
-        y = Year.objects.get(id=year_id)
+        y = get_object_or_404(Year, id=year_id)
         c = y.get_favorite_trimester().get_favorite_category()
         if c:
             return HttpResponse(json.dumps({'forward': c.get_url()}))
